f33ace75c9db0f4a550ea62cc28ba8c36180ca93
Andrey <andrey.nik.999@gmail.com>
2021-03-30 05:49:03 +0700

    Made login

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ApiClient.Models;
 
 namespace ApiClient.Controllers
 {
     public class ClientController : Controller
     {
-        // GET: Client
+        private readonly IClientModel _clientModel;
+        private readonly string _url = "http://api.pixlpark.com";
+
+        public ClientController()
+        {
+            _clientModel = new ClientModel();
+        }
+        //GET: Client
         public ActionResult Index()
         {
             return View();
         }
+
+        public void Login(string public_key, string private_key)
+        {
+            _clientModel.LogIn(_url, public_key, private_key);
+        }
+
+
     }
 }
---
+++ Models/IClientModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace ApiClient.Models
+{
+    public interface IClientModel
+    {
+        void LogIn(string url, string public_key, string private_key);
+    }
+}
